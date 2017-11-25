19f046d5ace7cab3fbef2b801941bd9b57dd8594
kpdev <kpdevelop@mail.ru>
2017-11-26 00:32:29 +0300

    Change in test 2

--- test/FigCircle.cpp
+++ test/FigCircle.cpp
@@ -9,14 +9,42 @@
 CREATE_REG_MARK_METHOD(FigCircle);
 REGISTER_SPECIALIZATION(Figure, FigCircle, "Registration of: FigCircle");
 
-INIT_v2(FigCircle);
+void Init(FigCircle& ft, int r) {
+  ft.mark = GetRegMarkFigCircle();
+  Circle& t = ft._spec;
+  Init(t, r);
+}
 
-v1_CREATE(FigCircle);
+// Создание фигуры-треугольника без инициализации сторон
+FigCircle* CreateFigCircle() {
+  FigCircle* pft = new FigCircle;
+  Init(*pft, 0);
+  return pft;
+}
 
-v1_IN(FigCircle, Circle);
-OUT(FigCircle, Circle);
+// Создание фигуры-треугольника с инициализаций сторон
+FigCircle* CreateFigCircleAndInit(int r) {
+  FigCircle* pft = CreateFigCircle();
+  Init(*pft, r);
+  return pft;
+}
 
-DELETE(FigCircle);
+// Ввод специализации фигуры-треугольника из потока
+void In(FigTriangle& ft, ifstream &ifst) {
+  Circle& t = ft._spec;
+  In(ifst, t);
+}
+
+// Вывод специализации фигуры-треугольника в поток
+void Out(FigCircle& ft, ofstream &ofst) {
+  ofst << "Circle is as Specialization of Figure: r = " << ft._spec.r << endl;
+}
+
+// Удаление специализации фигуры-треугольника
+void DeleteFigCircle(FigCircle& pft) {
+  delete &pft;
+}
+
 
 START_REGISTER_METHODS();
 REGISTER_METHOD_WITH_CHECK_RET_PTR(CreateFigureUseFileMark, CreateFigCircle, FigCircle, "Registration of CreateFigCircleUseFileMark", 3, Figure);

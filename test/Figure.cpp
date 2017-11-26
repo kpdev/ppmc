17eb356b434d6b004679a2fca05c5472125c0249
kpdev <kpdevelop@mail.ru>
2017-11-26 11:45:06 +0300

    Change in test 8

--- test/Figure.cpp
+++ test/Figure.cpp
@@ -1,6 +1,7 @@
 // Figure.cpp - Реализация функций, осуществляющих обработку обобщенной фигуры
 
 #include "Figure.h"
+#include <iostream>
 
 //------------------------------------------------------------------------------
 // Реализации глобальных переменных, обеспечивающих подключение зарегистрировнных функций
@@ -63,11 +64,11 @@ Figure* CreateFigureUseFileMark(int fileMark) {
 // Ввод значений полей фигуры-специализаии из потока через обобщенную функцию
 // после определения маркера фигуры из файла и создания конкретной фигуры
 void InFigureValue(ifstream &ifst, Figure& f) {
-    cout << "StartInfigure custom\n";
+    std::cerr << "StartInfigure custom\n";
     InFigureValueFunc func = InFigureValueFuncArray[f.mark];
-    cout << "StartInfigure custom 2\n";
+    std::cerr << "StartInfigure custom 2\n";
     func(f, ifst);
-    cout << "EndInfigure custom\n";
+    std::cerr << "EndInfigure custom\n";
 }
 
 // Ввод обобщенной фигуры из потока
